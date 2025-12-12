b4dca78988a4c0905d95eddc3bf79734d75bc519
MatthewScargill <matthewscargilluni@gmail.com>
2025-12-12 19:29:29 +0000

    dense and low rank block info and storage

--- src/hmatrix.rs
+++ src/hmatrix.rs
@@ -1,3 +1,5 @@
+use num_complex::Complex64;
+
 use crate::kernel::Kernel;
 use crate::cluster::ClusterTree;
 use crate::block::BlockTree;
@@ -5,6 +7,36 @@ use crate::block::BlockTree;
 
 // turning BlockTree into that sweet sweet Hmatrix 
 // also useful methods etc  
+
+
+// aptly named storage method for blocks
+pub enum BlockStorage {
+    Dense(DenseBlock),
+    LowRank(LowRankBlock),
+} // dense and lowrank for sorting during Hmatrix construction
+
+// high resolution block
+pub struct DenseBlock {
+    pub rows: Vec<usize>, // indices into target Nodes 
+    pub cols: Vec<usize>,  // indices into source Nodes
+    pub data: Vec<Complex64>, // going to store data in row major order ie. A00 -> A0n, A10 -> A1n, etc 
+    // Aij = data[i * len[cols] + j]
+}
+
+// approximation using A = UV^T, where U and V are essentially basis vectors since you can approximate the full Aij as basically linearly dependent 
+// ACA algorithm needed in eval
+pub struct LowRankBlock {
+    pub rows: Vec<usize>, // indices into target Nodes 
+    pub cols: Vec<usize>, // indices into source Nodes 
+
+    // keep rank < min(rows, columns) == number of columns in U and V, how much resolution do you want to keep?
+    pub rank: usize,
+
+    pub u: Vec<Complex64>, // len(rows) x rank matrix
+    pub v: Vec<Complex64>, // len(col) x rank matrix 
+    // row major as above 
+    
+}
 
 
 // wamt hmatrix to look something like this 
