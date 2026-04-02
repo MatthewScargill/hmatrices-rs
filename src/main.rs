9c25add78f7615bbb947ea49054eaf9804eb18d7
MatthewScargill <matthewscargilluni@gmail.com>
2026-04-02 15:18:34 +0200

    testing commits on nvim

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use hmats_rs::*;
 
-
-
 // random plotting function
 use plotters::prelude::*;
 fn plot(points: &[[f64; 2]]) {
@@ -12,23 +10,26 @@ fn plot(points: &[[f64; 2]]) {
         .build_cartesian_2d(-2.0..2.0, -2.0..2.0)
         .unwrap();
 
-    chart.draw_series(
-        points.iter().map(|p| Circle::new((p[0], p[1]), 2, RED.filled()))
-    ).unwrap();
+    chart
+        .draw_series(
+            points
+                .iter()
+                .map(|p| Circle::new((p[0], p[1]), 2, RED.filled())),
+        )
+        .unwrap();
 }
 
-
 fn main() {
+    use std::time::Instant;
+    let now = Instant::now();
 
-
-
-    const D: usize=3; // keeping this around for ease of testing but will kill off like with constructor for all top level functions
+    const D: usize = 3; // keeping this around for ease of testing but will kill off like with constructor for all top level functions
     let x = [0.0, 0.0];
     let y = [4.0, 0.0];
     let val = Laplace.eval(&x, &y);
     println!("Laplace Greens function = {:?}", val.re); // can chuck .re on it when using laplace 
 
-    // trying out the nodes 
+    // trying out the nodes
     let mut testpoints: Vec<[f64; 3]> = Vec::new();
     testpoints.push([0.0, 0.0, 2.0]);
     testpoints.push([0.4, 0.2, 0.3]);
@@ -41,16 +42,16 @@ fn main() {
     // ----------- EXAMPLE WORKFLOW - with fixed top level function implementation
     // how much can you reasonably ask from someone using your user
 
-    // importing points as vector 
+    // importing points as vector
     let ecn: Vec<[f64; 2]> = cardioid_nodes(20);
 
     // maybe a quick plot if we fancy
-    plot(&ecn);
+    // plot(&ecn);
 
     // creating dynamic nodes as top level type to interact with top level functions
     let examplenodes = DynamicNodes::D2(Nodes::new(ecn)); // this should be its own function in nodes 
 
-    // what kind of kernel do we fancy 
+    // what kind of kernel do we fancy
     //let kernelfunc = Helmholtz{wavenumber: 3.0};
     let kernelfunc = Laplace;
 
@@ -58,24 +59,19 @@ fn main() {
 
     // ----------------------------------
 
-    
-    
+    // this is me adding a little note in neovim
+
     //constructor(&card_nodes, Laplace);
     // let idx = [0,1,3];
     //let bboxtest: BBox<D> = nodetest.bbox_from_indices(&idx);
 
-
     //println!("min values of the bounding box = {:?}", bboxtest.min);
     //println!("centre of the bounding box = {:?}", bboxtest.centre());
-
-    use std::time::Instant;
-    let now = Instant::now();
 
     let testclustertree: ClusterTree<D> = ClusterTree::build_tree(&nodetest, 1);
     let _testblocktree: BlockTree = BlockTree::build_tree(&testclustertree, &testclustertree, 0.4);
 
     testclustertree.print();
-
 
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
